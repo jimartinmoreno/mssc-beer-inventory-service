593395b17b788b28d381005b6e9c84067aedb609
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2021-12-03 12:29:45 +0100

    Revision de código para usar colas ActiveMQ artemis

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -1,16 +1,14 @@
 package guru.sfg.beer.inventory.service;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeerInventoryApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(BeerInventoryApplication.class);
-        application.setBannerMode(Banner.Mode.CONSOLE);
-        application.setWebApplicationType(WebApplicationType.SERVLET);
+//        application.setBannerMode(Banner.Mode.CONSOLE);
+//        application.setWebApplicationType(WebApplicationType.SERVLET);
         application.run(args);
         // AnnotationConfigServletWebServerApplicationContext context = (AnnotationConfigServletWebServerApplicationContext) application.run(args);
         // SpringApplication.run(BeerInventoryApplication.class, args);
--- src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -22,7 +22,7 @@ public class NewInventoryListener {
     @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
     public void listen(NewInventoryEvent event){
 
-        log.debug("Got Inventory: " + event.toString());
+        log.debug("Got new INVENTORY request: " + event.toString());
 
         beerInventoryRepository.save(BeerInventory.builder()
                 .beerId(event.getBeerDto().getId())
